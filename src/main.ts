b65ccbec5fadc4337e67fe6c3e27555d625603d5
BeReyes1 <148413240+BeReyes1@users.noreply.github.com>
2025-10-06 22:30:44 +0000

    Added clicking logic, Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+   <div id="counterText">0 ducks</div>
 `;
 
 const clickButton = document.createElement("button");
 clickButton.textContent = "🦆";
 document.body.append(clickButton);
+
+const counterText = document.getElementById("counterText")!;
+
+clickButton.addEventListener("click", () => {
+  counter++;
+  counterText.textContent = `${counter} ducks`;
+});
