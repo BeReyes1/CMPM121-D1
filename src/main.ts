680e245fb3459e018a8394071ede621f17cdc840
BeReyes1 <148413240+BeReyes1@users.noreply.github.com>
2025-10-07 01:07:48 +0000

    3 upgrades added, auto counter+item tracker added, Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,61 @@ import "./style.css";
 let counter: number = 0;
 let growthRate: number = 0;
 
+let aItems: number = 0;
+let bItems: number = 0;
+let cItems: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
    <div id="counterText">0 ducks</div>
+   <div id="growthText"> 0 cookies/sec</div>
+   <div id= "itemsText"> 0 A, 0 B, 0 C</div>
 `;
 
 const clickButton = document.createElement("button");
 clickButton.textContent = "🦆";
 document.body.append(clickButton);
-const purchaseButton = document.createElement("button");
-purchaseButton.textContent = "Auto Click: Costs 10";
-document.body.append(purchaseButton);
+
+const purchaseButtonA = document.createElement("button");
+purchaseButtonA.textContent = "Auto Click A: Costs 10";
+document.body.append(purchaseButtonA);
+const purchaseButtonB = document.createElement("button");
+purchaseButtonB.textContent = "Auto Click B: Costs 100";
+document.body.append(purchaseButtonB);
+const purchaseButtonC = document.createElement("button");
+purchaseButtonC.textContent = "Auto Click C: Costs 1000";
+document.body.append(purchaseButtonC);
 
 const counterText = document.getElementById("counterText")!;
+const itemsText = document.getElementById("itemsText")!;
+const growthText = document.getElementById("growthText")!;
 
 clickButton.addEventListener("click", () => {
   counter++;
   counterText.textContent = `${counter.toFixed(0)} ducks`;
 });
 
-purchaseButton.addEventListener("click", () => {
+purchaseButtonA.addEventListener("click", () => {
   if (counter >= 10) {
     counter -= 10;
-    growthRate++;
+    growthRate += 0.1;
+    aItems++;
+  }
+});
+
+purchaseButtonB.addEventListener("click", () => {
+  if (counter >= 100) {
+    counter -= 100;
+    growthRate += 2;
+    bItems++;
+  }
+});
+
+purchaseButtonC.addEventListener("click", () => {
+  if (counter >= 1000) {
+    counter -= 1000;
+    growthRate += 50;
+    cItems++;
   }
 });
 
@@ -37,8 +69,13 @@ function autoStepClick(timestamp: number) {
 
   counter += elapsedTime * growthRate;
   counterText.textContent = `${counter.toFixed(0)} ducks`;
+  itemsText.textContent =
+    `${aItems} A Items, ${bItems} B Items, ${cItems} C Items`;
+  growthText.textContent = `${growthRate.toFixed(1)} ducks/second`;
 
-  purchaseButton.disabled = counter < 10;
+  purchaseButtonA.disabled = counter < 10;
+  purchaseButtonB.disabled = counter < 100;
+  purchaseButtonC.disabled = counter < 1000;
 
   requestAnimationFrame(autoStepClick);
 }
